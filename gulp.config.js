4c1bd5494caa7f7d56613f69e697ea10e237fc18
Yuriy Bevov <jury19y@mail.ru>
2021-07-03 11:52:32 +0300

    initial commit

---
+++ gulp.config.js
@@ -0,0 +1,43 @@
+const SOURCE_PATH = 'source/';
+const BUILD_PATH = 'build/';
+
+const PATHS = {
+
+  fonts: {
+    src: `${SOURCE_PATH}fonts/**/*.{woff,woff2}`,
+    output: `${BUILD_PATH}fonts/`,
+  },
+
+  images: {
+    src: `${SOURCE_PATH}img/static/**/*.{png,jpg,svg}`,
+    spriteSrc: `${SOURCE_PATH}/img/svg-sprite/icon-*.svg`,
+
+    dest: `${BUILD_PATH}img/static`,
+    spriteFileName: 'svg-sprite.svg',
+  },
+
+  html: {
+    src: `${SOURCE_PATH}pug/*.pug`,
+    srcWatch: `${SOURCE_PATH}pug/*.pug`,
+    dest: BUILD_PATH,
+  },
+
+  styles: {
+    src: `${SOURCE_PATH}styles/**/*.scss`,
+    dest: `${BUILD_PATH}css/`,
+    inputFileName: `${SOURCE_PATH}styles/style.scss`,
+    outputFileName: 'styles.min.css',
+  },
+
+  scripts: {
+    srcWatch: `${SOURCE_PATH}scripts/**/**/*.js`,
+    inputFileName: `${SOURCE_PATH}scripts/index.js`,
+    dest: `${BUILD_PATH}`,
+  },
+
+  vue: {
+    srcWatch: `${SOURCE_PATH}scripts/**/**/*.vue`
+  }
+};
+
+module.exports = { PATHS, BUILD_PATH }
