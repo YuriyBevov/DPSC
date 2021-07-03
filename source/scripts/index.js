cd35597fca747ac80dbd91b6d25e6d6bc057a58a
Yuriy Bevov <jury19y@mail.ru>
2021-07-03 20:46:08 +0300

    главная страница

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,18 @@
 'use strict'
 
+import Swiper from 'swiper'
 
-console.log('hedllo!')
+const sliderDelay = 3000;
+
+let slider = document.querySelector('.situations-swiper-container');
+
+if(slider) {
+    new Swiper('.situations-swiper-container', {
+        slidesPerView: 'auto',
+        spaceBetween: 10,
+    
+        autoplay: {
+          delay: sliderDelay,
+        }
+    });
+}
