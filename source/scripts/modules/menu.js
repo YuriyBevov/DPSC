05b5ae584b0f2c8f4a1db702ed9f97e925e9c75f
Yuriy Bevov <jury19y@mail.ru>
2021-07-04 14:50:32 +0300

    добавил меню

--- source/scripts/modules/menu.js
+++ source/scripts/modules/menu.js
@@ -1,1 +1,25 @@
-console.log('menu')
+const menu = document.querySelector('.menu');
+const menuOpenBtns = document.querySelectorAll('.js-menu-opener');
+const menuCloseBtn = document.querySelector('.js-menu-close-btn');
+
+const onClickOpenMenu = () => {
+    menu.classList.add('opened');
+
+    menuOpenBtns.forEach(btn => {
+        btn.removeEventListener('click', onClickOpenMenu);
+    });
+}
+
+const onClickCloseMenu = () => {
+    menu.classList.remove('opened');
+
+    menuOpenBtns.forEach(btn => {
+        btn.addEventListener('click', onClickOpenMenu);
+    });
+}
+
+menuOpenBtns.forEach(btn => {
+    btn.addEventListener('click', onClickOpenMenu);
+});
+
+menuCloseBtn.addEventListener('click', onClickCloseMenu);
