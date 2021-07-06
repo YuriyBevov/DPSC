b66374608f7d2c2259a0d569fe5312622d5e3819
Yuriy Bevov <jury19y@mail.ru>
2021-07-06 14:25:09 +0300

    модалка добавить компанию

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -12373,9 +12373,35 @@ showBtn.forEach(btn => {
 const menu = document.querySelector('.menu');
 const menuOpenBtns = document.querySelectorAll('.js-menu-opener');
 const menuCloseBtn = document.querySelector('.js-menu-close-btn');
-
+const menuItems = menu.querySelectorAll('.menu__link');
+const addCompanyModal = document.querySelector('.addCompanyModal')
+const addCompanyCloseBtn = addCompanyModal.querySelector('.js-addCompany-modal-close-btn');
+ 
 const onClickOpenMenu = () => {
     menu.classList.add('opened');
+
+    const onMenuItemClickHandler = (evt) => {
+        addCompanyModal.classList.add('opened')
+
+        menuItems.forEach(item => {
+            item.removeEventListener('click', onMenuItemClickHandler);
+        })
+
+        const onClickCloseAddCompanyModal = () => {
+            addCompanyModal.classList.remove('opened')
+            menuItems.forEach(item => {
+                item.addEventListener('click', onMenuItemClickHandler);
+            })
+
+            addCompanyCloseBtn.removeEventListener('click', onClickCloseAddCompanyModal)
+        }
+
+        addCompanyCloseBtn.addEventListener('click', onClickCloseAddCompanyModal)
+    }
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', onMenuItemClickHandler);
+    })
 
     menuOpenBtns.forEach(btn => {
         btn.removeEventListener('click', onClickOpenMenu);
