885b58415dc472b7c1b9dd21b1a4b0d40e407378
Yuriy Bevov <jury19y@mail.ru>
2021-07-06 13:07:27 +0300

    z3

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -12268,10 +12268,10 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-console.log('zoom2')
+console.log('zoom3')
 document.addEventListener('touchmove', function (evt) {
     if (evt.scale !== 1) { evt.scale = 1 }
-}, { passive: false });
+}, { passive: true }); 
 
 /***/ }),
 
