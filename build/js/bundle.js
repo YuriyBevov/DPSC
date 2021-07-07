5e3e259cee12a2c4457a390761d3a1a45e7ab657
Yuriy Bevov <jury19y@mail.ru>
2021-07-07 14:45:49 +0300

    js

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -12372,45 +12372,52 @@ showBtn.forEach(btn => {
 
 const openerBtns = document.querySelectorAll('.js-menu-opener');
 const menuModal = document.querySelector('.menuModal');
+const addCompanyModal = document.querySelector('.addCompanyModal');
+const addCompanyModalCloseBtn = addCompanyModal.querySelector('.js-addCompany-modal-close-btn');
+const menuModalItems = menuModal.querySelectorAll('.menuModal__item');
+const menuModalCloseBtn = menuModal.querySelector('.js-menuModal-close-btn');
 
 const onClickOpenMenu = () => { 
     menuModal.classList.add('opened')
-    const menuModalItems = menuModal.querySelectorAll('.menuModal__item');
+    
 
     const onMenuItemClickOpenAddCompanyModal = () => {
-
-        menuModalItems.forEach(item => {
-            item.removeEventListener('click', onMenuItemClickOpenAddCompanyModal)
-        })
-
-        const addCompanyModal = document.querySelector('.addCompanyModal');
-        const addCompanyModalCloseBtn = addCompanyModal.querySelector('.js-addCompany-modal-close-btn');
-
         addCompanyModal.classList.add('opened')
 
+
+        /* menuModalItems.forEach(item => {
+            
+            item.removeEventListener('click', onMenuItemClickOpenAddCompanyModal)
+        }) */
+
         const onAddCompanyModalCloseBtnClick = () => {
+            console.log('onAddCompanyModalCloseBtnClick')
             addCompanyModal.classList.remove('opened')
             addCompanyModalCloseBtn.removeEventListener('click', onAddCompanyModalCloseBtnClick)
 
-            menuModalItems.forEach(item => {
+            /* menuModalItems.forEach(item => {
+                console.log('menuModalItems remove event')
                 item.addEventListener('click', onMenuItemClickOpenAddCompanyModal)
-            })
+            }) */
         }
 
         addCompanyModalCloseBtn.addEventListener('click', onAddCompanyModalCloseBtnClick)
     }
 
     menuModalItems.forEach(item => {
+        console.log('menuModalItems add event')
         item.addEventListener('click', onMenuItemClickOpenAddCompanyModal)
     })
 
     openerBtns.forEach(btn => {
+        console.log('openerBtns remove event')
         btn.removeEventListener('click', onClickOpenMenu)
     })
 
-    const menuModalCloseBtn = menuModal.querySelector('.js-menuModal-close-btn');
+    
 
     const onClickCloseMenuModal = () => {
+        console.log('onClickCloseMenuModal')
         menuModal.classList.remove('opened')
         menuModalCloseBtn.removeEventListener('click', onClickCloseMenuModal)
 
@@ -12419,6 +12426,7 @@ const onClickOpenMenu = () => {
         })
 
         menuModalItems.forEach(item => {
+            console.log('menuModalItems remove event')
             item.removeEventListener('click', onMenuItemClickOpenAddCompanyModal)
         })
     }
@@ -12427,6 +12435,7 @@ const onClickOpenMenu = () => {
 }
 
 openerBtns.forEach(btn => {
+    console.log('openerBtns add event')
     btn.addEventListener('click', onClickOpenMenu)
 })
 
--- source/scripts/modules/menu.js
+++ source/scripts/modules/menu.js
@@ -1,44 +1,51 @@
 const openerBtns = document.querySelectorAll('.js-menu-opener');
 const menuModal = document.querySelector('.menuModal');
+const addCompanyModal = document.querySelector('.addCompanyModal');
+const addCompanyModalCloseBtn = addCompanyModal.querySelector('.js-addCompany-modal-close-btn');
+const menuModalItems = menuModal.querySelectorAll('.menuModal__item');
+const menuModalCloseBtn = menuModal.querySelector('.js-menuModal-close-btn');
 
 const onClickOpenMenu = () => { 
     menuModal.classList.add('opened')
-    const menuModalItems = menuModal.querySelectorAll('.menuModal__item');
+    
 
     const onMenuItemClickOpenAddCompanyModal = () => {
-
-        menuModalItems.forEach(item => {
-            item.removeEventListener('click', onMenuItemClickOpenAddCompanyModal)
-        })
-
-        const addCompanyModal = document.querySelector('.addCompanyModal');
-        const addCompanyModalCloseBtn = addCompanyModal.querySelector('.js-addCompany-modal-close-btn');
-
         addCompanyModal.classList.add('opened')
 
+
+        /* menuModalItems.forEach(item => {
+            
+            item.removeEventListener('click', onMenuItemClickOpenAddCompanyModal)
+        }) */
+
         const onAddCompanyModalCloseBtnClick = () => {
+            console.log('onAddCompanyModalCloseBtnClick')
             addCompanyModal.classList.remove('opened')
             addCompanyModalCloseBtn.removeEventListener('click', onAddCompanyModalCloseBtnClick)
 
-            menuModalItems.forEach(item => {
+            /* menuModalItems.forEach(item => {
+                console.log('menuModalItems remove event')
                 item.addEventListener('click', onMenuItemClickOpenAddCompanyModal)
-            })
+            }) */
         }
 
         addCompanyModalCloseBtn.addEventListener('click', onAddCompanyModalCloseBtnClick)
     }
 
     menuModalItems.forEach(item => {
+        console.log('menuModalItems add event')
         item.addEventListener('click', onMenuItemClickOpenAddCompanyModal)
     })
 
     openerBtns.forEach(btn => {
+        console.log('openerBtns remove event')
         btn.removeEventListener('click', onClickOpenMenu)
     })
 
-    const menuModalCloseBtn = menuModal.querySelector('.js-menuModal-close-btn');
+    
 
     const onClickCloseMenuModal = () => {
+        console.log('onClickCloseMenuModal')
         menuModal.classList.remove('opened')
         menuModalCloseBtn.removeEventListener('click', onClickCloseMenuModal)
 
@@ -47,6 +54,7 @@ const onClickOpenMenu = () => {
         })
 
         menuModalItems.forEach(item => {
+            console.log('menuModalItems remove event')
             item.removeEventListener('click', onMenuItemClickOpenAddCompanyModal)
         })
     }
@@ -55,5 +63,6 @@ const onClickOpenMenu = () => {
 }
 
 openerBtns.forEach(btn => {
+    console.log('openerBtns add event')
     btn.addEventListener('click', onClickOpenMenu)
 })
